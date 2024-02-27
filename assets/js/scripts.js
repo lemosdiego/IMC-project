9428f79065d8b8395f3a8f1cd3784bd3a757f09c
diego.lemos <57995631+lemosdiego@users.noreply.github.com>
2024-02-27 09:15:51 -0300

    Criação de tabela no projeto

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,12 @@
 
+const title =  document.getElementById('title')
+title.innerHTML = "Neste artigo Vamos falar um Pouco sobre o IMC"
+const subtitle = document.getElementById('subtitle')
+subtitle.innerHTML = "O que é o IMC?"
+const artigo = document.getElementById('artigo')
+artigo.innerHTML = " O Índice de Massa Corporal (IMC) é basicamente uma maneira de entender se alguém está com um peso saudável para a sua altura. Você divide o peso da pessoa pelo quadrado da altura dela. Isso dá uma ideia geral se a pessoa está abaixo do peso, dentro do peso normal, com sobrepeso ou obesa.Mas é importante lembrar que o IMC não conta toda a história. Ele não leva em consideração coisas como a quantidade de músculos que alguém tem, onde a gordura está localizada no corpo ou quão saudável alguém é em geral. Então, embora seja útil para começar a entender a saúde, não é a única coisa que importa."
+
+
 function calcular(){
     let altura = document.getElementById("altura")
     let peso = document.getElementById("peso")
